c6e83db898201109c59e95d67302bdb6e6f9ac17
Natalia Batista <nataliadominguesb@hotmail.com>
2025-03-27 20:32:16 -0300

    :tada:// se tiver light mode, adidionar a imagem do sol // se tiver light mode, adidionar a imagem do sol // se tiver light mode, adidionar a imagem do sol

---
+++ .vscode/script.js
@@ -0,0 +1,18 @@
+function toggleDarkMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegas a tag img
+  const img = document.querySelector("#profile img")}
+  // substituir a imagem
+  if (html.classList.toggle("light")) {
+    
+
+    img.setAttribute('src, ./assets/avatar-light.png')
+
+  }else { 
+      // se tiver light mode, adidionar a imagem do sol 
+
+
+  // se tiver dark mode, adicionar a imagem da lua
+  }
